ad9a87d77058741d7d796d80dc4e1676ed085c24
Rottschaefer <rottschaefer54@gmail.com>
2023-04-04 20:12:14 -0300

    aprofundamento SQL realizado

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -32,3 +32,78 @@ VALUES(02, "regata", 45, "blusas");
 
 INSERT INTO products(id, name, price, category)
 VALUES(03, "boné supreme", 7000, "para trouxas");
+
+--Aprofundamento SQL
+
+--retorna todos os usuários cadastrados
+
+SELECT * FROM users;
+
+--retorna todos os produtos cadastrados
+
+SELECT * FROM products;
+
+--crie um termo de busca, por exemplo "monitor"
+
+DECLARE @busca TEXT = 'monitor'; -- Definindo a variável 
+
+SELECT * FROM products
+WHERE name LIKE '%monitor%';
+
+--crie um novo usuário
+--insere o item mockado na tabela users
+
+INSERT INTO users(id, email, password)
+VALUES(04, "guigui@gmail.com", "212121"); 
+
+
+--crie um novo produto
+--insere o item mockado na tabela products
+
+INSERT INTO products(id, name, price, category)
+VALUES(06, "Gravata", 100, "Acessórios para Ternos");
+
+
+--busca de produtos por id
+
+SELECT * FROM products
+WHERE id = 6;
+
+--deleção de user por id
+
+DELETE FROM users
+WHERE id = 01;
+
+SELECT * FROM users;
+
+--deleção de produto por id
+
+DELETE FROM products
+WHERE id = 01;
+
+SELECT * FROM products;
+
+--edição de user por id
+
+UPDATE users
+SET password = "Atualizado"
+WHERE id = 2;
+
+--edição de produto por id
+
+UPDATE products
+SET name = "Atualizado"
+WHERE id = 2;
+
+
+SELECT * FROM users
+ORDER BY email DESC;
+
+SELECT * FROM products
+ORDER BY price DESC
+LIMIT 20
+OFFSET 0;
+
+SELECT * FROM products
+WHERE price > 100 AND price < 300
+ORDER BY price ASC;
